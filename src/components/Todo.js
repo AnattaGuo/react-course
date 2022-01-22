a6849f718d634e8f5eec2f5df9bcda459afefe2c
anattaguo <mengjunkwok@gmail.com>
2022-01-22 22:04:51 +0800

    Handling Events

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,15 @@
 function Todo(props) { 
+    function  deleteHandler() {
+        
+
+    }
+
+
     return (
         <div className='card'>
             <h2>{props.text}</h2>
             <div className='actions'>
-                <button className='btn'>Delete</button>
+                <button className='btn' onClick={deleteHandler}>Delete</button>
             </div>
         </div>
     )
@@ -13,4 +19,11 @@ function Todo(props) {
 export default Todo
 
 // Note:
-// props concept for accepting data from outside
+// Props: props concept for accepting data from outside. In this case, the props accept data from App.js <Todo />
+// onClick: tell the react you want to react to a click on that element
+// onClick={function() {}} :  the value you assign for this attribute is not some text, not a string, but a dynamic expression (`onClick = { }`), which should hold a function, that should be executed when a click occurs.
+// here we could define an anonymous function like `function () {}`
+// or with the arrow `onClick={() => {}} ` ( is the same as `onClick={function() {}}`)
+
+
+// Add function deleteHandler
